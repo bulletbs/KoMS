81af8d7aa1b31d1a85c489f93ab0c16069cbde2e
Butch Stine <bulletua@gmail.com>
2015-06-02 19:07:48 +0300

    - added NewLine element in CRUD List sorter

--- views/admin/crud/sort.php
+++ views/admin/crud/sort.php
@@ -1,10 +1,14 @@
 <?php defined('SYSPATH') or die('No direct script access.');?>
 <?=Form::open(null, array('method'=>'get', 'class'=>'well form-inline', 'enctype'=>'multipart/form-data', 'id'=>'sortForm'))?>
 <?foreach($sort_fields as $field => $field_data):?>
+    <? if($field_data['type'] == 'nl'): ?>
+        <br>
+        <?continue?>
+    <?endif?>
     <?= Form::label($field, $field_data['label'], array('class'=>'control-label')) ?>
 
     <? if($field_data['type'] == 'text'): ?>
-        <?= Form::input($field, $field_data['data'], array('id'=>'sortValue')) ?>
+        <?= Form::input($field, $field_data['data'], array('id'=>'sortValue small')) ?>
 
     <? elseif($field_data['type'] == 'checkbox'): ?>
         <?= Form::checkbox($field, 1, $model->{$field}>0 ? TRUE : FALSE, array('id'=>'sortValue')) ?>
