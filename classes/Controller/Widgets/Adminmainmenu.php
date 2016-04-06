3c8ef8ac39aef4436f25e1035eb7a32e2c398f3d
Butch Stine <bulletua@gmail.com>
2016-04-06 20:31:50 +0300

    меню админки вынесено в конфигурацию

--- classes/Controller/Widgets/Adminmainmenu.php
+++ classes/Controller/Widgets/Adminmainmenu.php
@@ -8,61 +8,7 @@ class Controller_Widgets_Adminmainmenu extends Controller_System_Widgets {
 
     public function action_index()
     {
-
-        $menu = array(
-            array(
-                'controller'=>'pages',
-                'icon'=>'file',
-                'label'=>__('Pages'),
-            ),
-//            array(
-//                'controller'=>'news',
-//                'icon'=>'calendar',
-//                'label'=>__('News'),
-//            ),
-            array(
-                'controller'=>'catalog',
-                'icon'=>'briefcase',
-                'label'=>__('Site Catalog'),
-            ),
-//            array(
-//                'controller'=>'news',
-//                'icon'=>'calendar',
-//                'label'=>__('News'),
-//            ),
-//            array(
-//                'controller'=>'catalog',
-//                'icon'=>'briefcase',
-//                'label'=>__('Site Catalog'),
-//            ),
-            array(
-                'controller'=>'board',
-                'icon'=>'briefcase',
-                'label'=>__('Site Ads'),
-            ),
-            array(
-                'controller'=>'comments',
-                'icon'=>'comment',
-                'label'=>__('Comments'),
-            ),
-            array(
-                'controller'=>'users',
-                'icon'=>'user',
-                'label'=>__('Users'),
-            ),
-//            array(
-//                'icon'=>'file',
-//                'label'=> __('Contents'),
-//                'submenu'=>array(
-//                ),
-//            ),
-//            array(
-//                'icon'=>'folder-open',
-//                'label'=>'Data',
-//                'submenu'=>array(
-//                ),
-//            ),
-        );
+        $menu = Kohana::$config->load('koms')->admin_menu;
 
         $current_user = NULL;
         if($this->current_user)
---
+++ config/koms.php
@@ -0,0 +1,48 @@
+<?php
+
+return array(
+    'admin_menu' => array(
+        array(
+            'controller'=>'pages',
+            'icon'=>'file',
+            'label'=>__('Pages'),
+        ),
+        array(
+            'controller'=>'news',
+            'icon'=>'calendar',
+            'label'=>__('News'),
+        ),
+        array(
+            'controller'=>'catalog',
+            'icon'=>'briefcase',
+            'label'=>__('Site Catalog'),
+        ),
+        array(
+            'controller'=>'board',
+            'icon'=>'briefcase',
+            'label'=>__('Site Ads'),
+        ),
+        array(
+            'controller'=>'comments',
+            'icon'=>'comment',
+            'label'=>__('Comments'),
+        ),
+        array(
+            'controller'=>'users',
+            'icon'=>'user',
+            'label'=>__('Users'),
+        ),
+//            array(
+//                'icon'=>'file',
+//                'label'=> __('Contents'),
+//                'submenu'=>array(
+//                ),
+//            ),
+//            array(
+//                'icon'=>'folder-open',
+//                'label'=>'Data',
+//                'submenu'=>array(
+//                ),
+//            ),
+    ),
+);
